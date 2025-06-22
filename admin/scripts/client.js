2618525b9afe5bd6fcd18367371a1b81bd9966a9
edinorog007 <151943734+edinorog007@users.noreply.github.com>
2025-06-22 20:59:08 +0700

    Update client.js

--- admin/scripts/client.js
+++ admin/scripts/client.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('https://edinorog007.github.io/diploma_project/admin/login', {
+            const response = await fetch('https://edinorog007.github.io/diploma_project/admin/orders', {
                 method: 'POST',
                 credentials: 'include',
                 headers: {
